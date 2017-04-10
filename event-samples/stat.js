2756f6c0dd9bc0edcd0545a4d41c3736a61ff134
Yi-Hua Liang <yihua1218@gmail.com>
2017-04-10 20:58:34 +0800

    Updated event-samples.

--- event-samples/stat.js
+++ event-samples/stat.js
@@ -12,6 +12,7 @@
 const xim_config = require('../.xim_config.js');
 
 const my_quantum_token = xim_config.quantum_token;
+const my_device_id = xim_config.device_id;
 
 module.exports = {
   method: 'stat',
@@ -19,5 +20,5 @@ module.exports = {
   xim_channel: 'lifx',
   xim_channel_set: 0,
   quantum_token: my_quantum_token,
-  device_id: 'id:d073d511fe04',
+  device_id: my_device_id,
 };
